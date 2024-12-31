3d99751d417d89ace7b1469136a072844f131dc0
Noa Santos <noasantos@MacBook-Air-de-Noa.local>
2024-12-31 13:15:37 +0000

    fix: remove content path for AgilizzaUI plugin in Tailwind CSS configuration

--- src/utils/plugin/index.ts
+++ src/utils/plugin/index.ts
@@ -2,7 +2,6 @@ import plugin from 'tailwindcss/plugin';
 import { colorPalette, defaultPrimary, defaultSecondary } from './colors';
 
 const AgilizzaUI = plugin(() => {}, {
-   content: ['./node_modules/agilizzapsi-ui/**/*.{js,ts,jsx,tsx}'],
    theme: {
       extend: {
          colors: {
