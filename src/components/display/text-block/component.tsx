95630db7ab006957329a3c44f39ce5eeabdb72fe
Noa Santos <noasantos@MacBook-Air-de-Noa.local>
2024-12-31 14:26:04 +0000

    fix: adjust max-width class order for TextBlock component

--- src/components/display/text-block/component.tsx
+++ src/components/display/text-block/component.tsx
@@ -4,7 +4,7 @@ import { TextBlockProps } from './types';
 
 export const TextBlock: React.FC<TextBlockProps> = ({ title, text, align = 'left' }) => {
    return (
-      <div className="flex w-full flex-col gap-y-1 md:max-w-[438px] xl:max-w-[468px]">
+      <div className="flex w-full max-w-[438px] flex-col gap-y-1 xl:max-w-[468px]">
          <Typography variant="h5" align={align}>
             {title}
          </Typography>
